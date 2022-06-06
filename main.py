85bb2b4e737ca82affbbb0bbb379675654e60346
HansjoergJaeggi <68072170+HansjoergJaeggi@users.noreply.github.com>
2022-06-06 21:14:29 +0200

    Aktualisiere main.blocks, main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 Zufallszahl = 0
+"""
+
+Aufgabe 5
+
+Wenn der Zustand wahr ist, sollte eine zufällige Zahl durch 5 dividiert werden. Wenn diese zufällige Zahl teilbar ist, dann sollte ein Herz angezeigt werden.
+
+"""
 # Rest von "Zufallszahl" /5=0
 # 
 # ist nicht direkt verfügbar. Wird in Python Zufallszahl % 5 == 0: geschrieben, wird der Block "Rest von Zufallszahl gebildet.
